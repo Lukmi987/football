97ea6d4f9620b04f944d4a024a74eca2498b1c97
Lukas <>
2021-03-17 20:19:41 +0100

    playing with loops

--- src/app/event/sagas/fetchOccurrences.js
+++ src/app/event/sagas/fetchOccurrences.js
@@ -2,6 +2,7 @@ import { put, select } from "redux-saga/effects";
 import axios from "../../axios-football";
 import { SET_EVENT } from "../../constants/actionTypes";
 import { loadEvents } from "../../helpers/eventHelpers";
+import _ from 'lodash'
 
 export function* fetchOccurrences() {
   try {
@@ -9,19 +10,55 @@ export function* fetchOccurrences() {
     const response = yield axios.get(`/occurrences.json?auth=${userToken}`);
 
     const entr = Object.entries(response.data);
-    console.log("pred flat", entr);
-    const flat = entr.flatMap((obj) => obj[1]);
-    console.log("shit", flat);
-    debugger;
-    const occ = entr.reduce(loadOccurrences, []);
-    console.log("entr", occ);
-    const data = response.data;
-    data.map((occur) => occur.creationTime);
+    //console.log("pred flat entries", entr);
+    const copyEnter = _.cloneDeep(entr)
+    const acumm = []
+    const some = (array) => {
 
-    const entries = Object.entries(response.data);
-    console.log("2 entries", entries);
-    const events = entries.reduce(loadEvents, []);
-    yield put({ type: SET_EVENT, data: events });
+
+      for (let i = 0; i < array.length; i++) {
+        let test = {}
+        let nevim;
+        for (let j = 0; j < array[i].length; j++) {
+          const t = (j === 0) ? "id" : 'neco'
+          let f = array[i][j]
+          if(j === 0) {
+            test.id = array[i][j]
+          } else if (j === 1){
+            for (let k = 0; k < f.length; k++) {
+              nevim = {...test, ...f[k]}
+              acumm.push(nevim)
+            }
+          }
+        }
+
+      }
+
+    }
+    some(copyEnter)
+    console.log(acumm, '.....test.....')
+
+    const flat = copyEnter.flatMap((el) => {
+     let len = el[1].length
+      const obj = {...el[1]}
+     for (let i = 0; i < len; i++) {
+       obj[i].id = el[0]
+     }
+      return obj
+    } ); //({...obj[1], id: obj[0]})
+    console.log("flat data...........", flat.flatMap((el) => el.cities));
+
+
+     debugger;
+    // const occ = entr.reduce(loadOccurrences, []);
+    // console.log("entr", occ);
+    // const data = response.data;
+    // data.map((occur) => occur.creationTime);
+    //
+    // const entries = Object.entries(response.data);
+    // console.log("2 entries", entries);
+    // const events = entries.reduce(loadEvents, []);
+    // yield put({ type: SET_EVENT, data: events });
   } catch (e) {
     console.log(e);
   }
